5b0baff3df782ae43c3f6f9cf3fa0b46236c8c80
James Waples <james@wapl.es>
2019-05-19 20:25:16 +0100

    Fmt

--- types.ts
+++ types.ts
@@ -1,19 +1,20 @@
-import * as T from "io-ts";
-import { Option, None, Some } from "funfix";
+import * as T from 'io-ts';
+import { PartialType, InterfaceType } from 'io-ts';
+import { Option, None, Some } from 'funfix';
 
 const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
 export const uuidDef = T.refinement(
   T.string,
-  str => uuidRegex.test(str),
-  "UUID"
+  (str) => uuidRegex.test(str),
+  'UUID',
 );
 export type Uuid = T.TypeOf<typeof uuidDef>;
 
 const isoDateRegex = /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)$/;
 export const isoDateDef = T.refinement(
   T.string,
-  str => isoDateRegex.test(str),
-  "ISO-8601"
+  (str) => isoDateRegex.test(str),
+  'ISO-8601',
 );
 export type IsoDate = T.TypeOf<typeof isoDateDef>;
 
@@ -25,19 +26,19 @@ export type OptionCodec<C extends T.Mixed> = T.Type<
 
 export function optionDef<C extends T.Mixed>(
   codec: C,
-  name: string = `Option<${codec.name}>`
+  name: string = `Option<${codec.name}>`,
 ): OptionCodec<C> {
   return new T.Type(
     name,
     (u): u is Option<T.TypeOf<C>> => u instanceof Option,
     (u, c) =>
       Option.of(u)
-        .map(value => {
+        .map((value) => {
           return codec
             .validate(value, c)
-            .chain(value => T.success(Option.of(value)));
+            .chain((value) => T.success(Option.of(value)));
         })
         .getOrElse(T.success(None)),
-    a => a.getOrElse(null)
+    (a) => a.getOrElse(null),
   );
 }

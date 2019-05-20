9e8a69b38f6c743b33d346bbc94ab001b89811a9
James Waples <james@wapl.es>
2019-05-20 14:24:48 +0100

    Fmt

--- events.ts
+++ events.ts
@@ -3,30 +3,30 @@ import * as T from 'io-ts';
 import { eventDef } from './types';
 
 export const oneEventDef = eventDef(
-    T.literal('one'),
-    T.literal('Event'),
-    T.literal('something'),
-    T.interface({
-        foo: T.string
-    })
+  T.literal('one'),
+  T.literal('Event'),
+  T.literal('something'),
+  T.interface({
+    foo: T.string
+  })
 );
 
 export const secondThingDef = eventDef(
-    T.literal('second'),
-    T.literal('Thing'),
-    T.literal('something'),
-    T.interface({
-        foo: T.string
-    })
+  T.literal('second'),
+  T.literal('Thing'),
+  T.literal('something'),
+  T.interface({
+    foo: T.string
+  })
 );
 
 export const thirdBlahDef = eventDef(
-    T.literal('third'),
-    T.literal('Blah'),
-    T.literal('something_else'),
-    T.interface({
-        foo: T.string
-    })
+  T.literal('third'),
+  T.literal('Blah'),
+  T.literal('something_else'),
+  T.interface({
+    foo: T.string
+  })
 );
 
 export type OneEvent = T.TypeOf<typeof oneEventDef>;
@@ -38,21 +38,21 @@ export type ThirdBlah = T.TypeOf<typeof thirdBlahDef>;
 import { Some, None } from 'funfix';
 
 const thing: OneEvent = {
-    id: '',
-    sequence_number: 0,
-    event_namespace: 'one',
-    event_type: 'Event',
-    entity_id: '',
-    entity_type: 'something',
-    created_at: '',
-    context: {
-        subject_id: '',
-        hostname: '',
-        username: '',
-        ip: None,
-        purge: None
-    },
-    data: Some({
-        foo: 'bar'
-    })
+  id: '',
+  sequence_number: 0,
+  event_namespace: 'one',
+  event_type: 'Event',
+  entity_id: '',
+  entity_type: 'something',
+  created_at: '',
+  context: {
+    subject_id: '',
+    hostname: '',
+    username: '',
+    ip: None,
+    purge: None
+  },
+  data: {
+    foo: 'bar'
+  }
 };
